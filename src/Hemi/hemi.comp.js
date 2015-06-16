04e983cb4a963c2ad7b221168885fc643e10e8be
Stephen Cote <sw.cote@gmail.com>
2015-06-15 21:45:48 -0700

    Build revision

--- src/Hemi/hemi.comp.js
+++ src/Hemi/hemi.comp.js
@@ -2,7 +2,7 @@
 /*
 	"Hemi" JavaScript Framework
 	Engine for Web Applications 3.5
-	Composite file "hemi.comp.js" release: 0.0.1321
+	Composite file "hemi.comp.js" release: 0.0.1497
 	Copyright 2002 - 2013. All Rights Reserved.
 	Author: Stephen W. Cote
 	Project: https://github.com/StephenCote/Hemi

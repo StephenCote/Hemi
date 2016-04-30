2e9bdfad057d3286a7c58582a3595a00a67637b1
StephenCote <Steve@cotedesk.local>
2016-04-29 21:40:49 -0700

    Add wildfly support

--- src/HemiFramework/repack.sh
+++ src/HemiFramework/repack.sh
@@ -2,4 +2,5 @@ cd ../PackageLibrary/ext
 java -cp .:./packagelibrary.jar:./lib/commons-cli-1.2.jar:./lib/objects-0.0.1-SNAPSHOT.jar:./lib/util-0.0.1-SNAPSHOT.jar:./lib/log4j-1.2.6.jar:./lib/packageobjects-0.0.1-SNAPSHOT.jar org.cote.pkglib.shell.PackageConfigurator -scriptConfig ~/Projects/Source/Web/Projects/Hemi/config.xml -packageFile ~/Projects/Source/Web/Projects/Hemi/HemiFramework.package.xml 
 cp export/HemiFramework/hemi.comp.js ~/Projects/Source/Web/Projects/Hemi/
 cd ../../HemiFramework
-mvn clean package jboss-as:redeploy
+mvn clean package wildfly:undeploy
+mvn wildfly:deploy

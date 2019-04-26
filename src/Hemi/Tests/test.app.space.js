ce56d6191bd6a1dde2e8eae4e3f455a5b5f6d1e4
Stephen Cote <sw.cote@gmail.com>
2019-04-26 08:00:23 -0700

    Hemi 4 Prep: Core package and object updates

--- src/Hemi/Tests/test.app.space.js
+++ src/Hemi/Tests/test.app.space.js
@@ -1,17 +1,14 @@
-Hemi.include("hemi.app");
-Hemi.include("hemi.app.comp");
-Hemi.include("hemi.data.form");
-Hemi.include("hemi.data.validator");
-
+this.dependencies.push("hemi.app","hemi.app.comp","hemi.data.form","hemi.data.validator");
 function TestInlineSpace(oTest){
 	var local_assert = this.Assert;
-	var oSpace = Hemi.app.createApplicationSpace(null, null, null, (function(oService, oSpace){
+	Hemi.app.createApplicationSpace(null, null, null, (function(oService, oSpace){
 		var sId = oSpace.space_id;
 
 		oService.clearAppSpace(oSpace);
 		var oTestSpace = oService.getSpace(sId);
 		
 		local_assert(!oTestSpace, "Test Space was not cleaned up");
-		
+		EndTestInlineSpace(true);
 	}));
+	return false;
 }

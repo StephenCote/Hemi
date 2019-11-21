e36e44d48cf828234d731581bb2cea4a7e54c3cf
Stephen Cote <sw.cote@gmail.com>
2019-11-21 10:04:03 -0800

    Maven build dependency updates

--- src/HemiFramework/repack.bat
+++ src/HemiFramework/repack.bat
@@ -1,8 +1,9 @@
 @echo off
 echo Convenience script to repack and rebundle the Hemi JavaScript Framework
 echo *** NOTE *** the paths are environment specific here, so change them as needed
+REM --add-modules java.xml.bind 
 cd C:\Users\swcot\Documents\GitHub\Hemi\src\configurator\bin
-call java --add-modules java.xml.bind -classpath .;./packagelibrary.jar;./lib/commons-cli-1.2.jar;./lib/objects-0.0.1-SNAPSHOT.jar;./lib/util-0.0.1-SNAPSHOT.jar;./lib/log4j-1.2.6.jar;./lib/packageobjects-0.0.1-SNAPSHOT.jar org.cote.pkglib.shell.PackageConfigurator -scriptConfig "C:\Users\swcot\Documents\GitHub\Hemi\src\Hemi\config.xml" -packageFile "C:\Users\swcot\Documents\GitHub\Hemi\src\Hemi\HemiFramework.package.xml" 
+call java -classpath .;./packagelibrary.jar;./lib/commons-cli-1.2.jar;./lib/objects-0.0.1-SNAPSHOT.jar;./lib/util-0.0.1-SNAPSHOT.jar;./lib/jaxb-runtime-2.3.2.jar;./lib/log4j-api-2.12.1.jar;./lib/log4j-core-2.12.1.jar;./lib/packageobjects-0.0.1-SNAPSHOT.jar org.cote.pkglib.shell.PackageConfigurator -scriptConfig "C:\Users\swcot\Documents\GitHub\Hemi\src\Hemi\config.xml" -packageFile "C:\Users\swcot\Documents\GitHub\Hemi\src\Hemi\HemiFramework.package.xml" 
 copy /y export\HemiFramework\hemi.comp.js C:\Users\swcot\Documents\GitHub\Hemi\src\Hemi
 cd c:\Users\swcot\workspace\HemiFramework
 call mvn clean package
